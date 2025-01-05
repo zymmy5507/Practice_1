e283a86d9a55e7cd08d999efbf52c3378cb53c0a
Varun Sharma <96142345+zymmy5507@users.noreply.github.com>
2025-01-05 19:21:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    window.location.href = 'https://www.geeksforgeeks.org/problems/search-in-a-matrix17201720/1';
+});
+document.getElementById('btn2').addEventListener('click', function() {
+    window.location.href = 'https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1';
+});
+document.getElementById('btn3').addEventListener('click', function() {
+    window.location.href = 'https://www.geeksforgeeks.org/problems/largest-zigzag-sequence5416/1';
+});
+document.getElementById('btn4').addEventListener('click', function() {
+    window.location.href = 'https://www.geeksforgeeks.org/problems/geek-and-its-colored-strings1355/1';
+});
+document.getElementById('btn5').addEventListener('click', function() {
+    window.location.href = 'https://www.geeksforgeeks.org/problems/anagram-1587115620/1';
+});
